0566cb1e9dd102f84061a5855f39c3e002861523
kyeling <60207452+kyeling@users.noreply.github.com>
2020-09-14 13:34:41 -0700

    Create MaximumDepthofBinaryTreeRecursive.c

---
+++ nevin/MaximumDepthofBinaryTreeRecursive.c
@@ -0,0 +1,24 @@
+int max(int (*a)(struct TreeNode*), int (*b)(struct TreeNode*)){
+    if(*a > *b) return *a;
+    return *b;
+}
+
+int maxDepth(struct TreeNode* root){
+    if(root == NULL) return 0;
+    if(root->left == NULL && root->right == NULL) return 1;
+    if(root->left == NULL) return 1 + maxDepth(root->right);
+    if(root->right == NULL) return 1 + maxDepth(root->left);
+    
+    return 1 + max(maxDepth(root->left), maxDepth(root->right));
+}
+
+/**
+options:
+- traverse every path and keep track of max length
+- recursion: if leaf, add 1, else take max of left and right subtrees
+
+the following don't work bc tree isn't balanced:
+- convert to array and use log on length of array
+- push all elements into a queue/linked list and use size()
+- recursion with static variable?
+**/
