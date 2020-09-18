1866e4ce79caccba679ddc692713b40ac2a7b850
kyeling <60207452+kyeling@users.noreply.github.com>
2020-09-17 18:38:50 -0700

    Update MaximumDepthofBinaryTreeRecursive.c
    better solution

--- nevin/MaximumDepthofBinaryTreeRecursive.c
+++ nevin/MaximumDepthofBinaryTreeRecursive.c
@@ -5,10 +5,6 @@ int max(int (*a)(struct TreeNode*), int (*b)(struct TreeNode*)){
 
 int maxDepth(struct TreeNode* root){
     if(root == NULL) return 0;
-    if(root->left == NULL && root->right == NULL) return 1;
-    if(root->left == NULL) return 1 + maxDepth(root->right);
-    if(root->right == NULL) return 1 + maxDepth(root->left);
-    
     return 1 + max(maxDepth(root->left), maxDepth(root->right));
 }
 
