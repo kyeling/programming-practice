00622fe0f4fa8aa92af207b463a9ef3f6f0cc166
kyeling <60207452+kyeling@users.noreply.github.com>
2020-09-22 16:20:59 -0700

    Create ReverseInteger.java

---
+++ strings/ReverseInteger.java
@@ -0,0 +1,38 @@
+// non-math method
+
+class Solution {
+    public int reverse(int x) {
+        
+        char[] arr = Integer.toString(x).toCharArray();
+        int left = 0;
+        int right = arr.length - 1;
+        
+        if (arr[0] == '-') {
+            left++;
+        }
+        
+        char temp = ' ';
+        while (left < right) {
+            temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+        
+        long rev = Long.valueOf(String.valueOf(arr));
+        if (rev > Integer.MIN_VALUE && rev < Integer.MAX_VALUE) {
+            return (int)rev;
+        }
+        return 0;
+        
+    }
+}
+
+/**
+convert int to string
+check for neg sign
+while left < right
+    switch left and right with temp digit
+convert to string
+**/
