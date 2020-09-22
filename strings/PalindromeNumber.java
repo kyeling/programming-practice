aadc3d707429a48b4c5051b470483bf62c870d68
kyeling <60207452+kyeling@users.noreply.github.com>
2020-09-22 16:19:23 -0700

    Create PalindromeNumber.java

---
+++ strings/PalindromeNumber.java
@@ -0,0 +1,17 @@
+// non-math method by converting int to string:
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        String str = Integer.toString(x);
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+}
