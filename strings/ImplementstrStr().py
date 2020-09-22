0c7da2c2919aaa9abca662a03f47962d3093e873
kyeling <60207452+kyeling@users.noreply.github.com>
2020-09-22 16:15:54 -0700

    Create ImplementstrStr().py

---
+++ strings/ImplementstrStr().py
@@ -0,0 +1,9 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        h = len(haystack)
+        n = len(needle)
+        if h == 0 and n == 0: return 0
+        for i in range(h - n + 1):
+            if haystack[i:i+n] == needle:
+                return i
+        return -1
