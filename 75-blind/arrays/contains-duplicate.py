8b51619f3637eb2f94d899433d4cd823f4e1b6f3
kyeling <60207452+kyeling@users.noreply.github.com>
2020-09-28 11:33:04 -0700

    Update contains-duplicate.py
    one-line solution

--- 75-blind/arrays/contains-duplicate.py
+++ 75-blind/arrays/contains-duplicate.py
@@ -1,10 +1,13 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        n = len(nums)
-        seen = set({})
-        if n == 0 or n == 1: return False
-        for num in nums:
-            if num in seen:
-                return True
-            seen.add(num)
-        return False
+        return len(set(nums)) < len(nums)
+    
+    # previous solution:
+        # n = len(nums)
+        # seen = set({})
+        # if n == 0 or n == 1: return False
+        # for num in nums:
+        #     if num in seen:
+        #         return True
+        #     seen.add(num)
+        # return False
