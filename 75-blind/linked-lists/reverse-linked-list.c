c648402e80c61fd65fa4b377807dd967beba0737
kyeling <60207452+kyeling@users.noreply.github.com>
2020-09-29 19:56:43 -0700

    Update reverse-linked-list.c
    added recursive solution

--- 75-blind/linked-lists/reverse-linked-list.c
+++ 75-blind/linked-lists/reverse-linked-list.c
@@ -1,14 +1,23 @@
 // iterative solution: time O(n), space O(1)
-//
-// struct ListNode* reverseList(struct ListNode* head){
-//     if(head == NULL || head->next == NULL) return head;
-//     struct ListNode* curr;
-//     struct ListNode* revHead = NULL;
-//     while(head != NULL){
-//         curr = head;
-//         head = head->next;
-//         curr->next = revHead;
-//         revHead = curr;
-//     }
-//     return revHead;
-// }
+
+struct ListNode* reverseList(struct ListNode* head){
+    if(head == NULL || head->next == NULL) return head;
+    struct ListNode* curr;
+    struct ListNode* revHead = NULL;
+    while(head != NULL){
+        curr = head;
+        head = head->next;
+        curr->next = revHead;
+        revHead = curr;
+    }
+    return revHead;
+}
+
+// recursive solution: time and space O(n)
+struct ListNode* reverseList(struct ListNode* head){
+    if(head == NULL || head->next == NULL) return head;
+    struct ListNode* curr = reverseList(head->next);
+    head->next->next = head;
+    head->next = NULL;
+    return curr;
+}
