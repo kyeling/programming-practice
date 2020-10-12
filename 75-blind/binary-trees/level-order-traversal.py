9f715e053c113cd289bb85bef98f74ae385a6839
kyeling <60207452+kyeling@users.noreply.github.com>
2020-10-11 19:04:23 -0700

    Update level-order-traversal.py

--- 75-blind/binary-trees/level-order-traversal.py
+++ 75-blind/binary-trees/level-order-traversal.py
@@ -23,3 +23,34 @@ class Solution:
             level.clear()
         
         return levels
+    
+    
+# Jaden's solution
+# can also use stack and pop
+# time O(V+E) or O(2n) if recursive, but O(n) otherwise, where n is number of nodes
+# space O(V+E) 
+from collections import deque
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root: return []
+        
+        nodes = []
+        
+        # initialize queue
+        nodeDeque = deque([root])
+        
+        #bfs until queue empty
+        while nodeDeque:
+            levellen = len(nodeDeque)
+            levelnodes = []
+            for i in range(levellen):
+                curNode = nodeDeque.popleft()
+                levelnodes.append(curNode.val)
+                
+                if curNode.left:
+                    nodeDeque.append(curNode.left)
+                if curNode.right:
+                    nodeDeque.append(curNode.right)
+                    
+            nodes.append(levelnodes)
+        return nodes
