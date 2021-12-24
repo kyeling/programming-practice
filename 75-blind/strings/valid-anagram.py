1dafa828a719eb14a7ac260d936bacd3c83e415c
kyeling <60207452+kyeling@users.noreply.github.com>
2021-12-24 01:04:53 -0800

    Update with faster solution

--- 75-blind/strings/valid-anagram.py
+++ 75-blind/strings/valid-anagram.py
@@ -1,6 +1,9 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        return Counter(s) == Counter(t)
+    
+    ## OLD solution
+        # return sorted(s) == sorted(t)
 
 # sorting: nlogn
 # hash map: O(n) space, O(n) time
